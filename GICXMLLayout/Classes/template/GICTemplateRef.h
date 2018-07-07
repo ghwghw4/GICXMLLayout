6327e660f4b76ff70a5dfe9dced3fe20bdc4cc73
gonghaiwei <dagehaoshuang@163.com>
2018-07-07 20:29:53 +0800

    no message

--- GICXMLLayout/Classes/template/GICTemplateRef.h
+++ GICXMLLayout/Classes/template/GICTemplateRef.h
@@ -8,6 +8,10 @@
 #import <Foundation/Foundation.h>
 #import "GICTemplate.h"
 
+/**
+ 模板引用。支持slot
+ slot 的使用可能会引起性能问题，主要是在for循环或者 list中使用的时候。因为会重复执行文本解析
+ */
 @interface GICTemplateRef : NSObject<LayoutElementProtocol>{
     NSMutableDictionary<NSString *, NSString *> *slotsXmlDocMap;
     

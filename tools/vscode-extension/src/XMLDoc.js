000960508cb6dc0bd5144a3f7ce1926f4d052704
龚海伟 <dagehaoshuang@163.com>
2018-12-21 16:29:22 +0800

    更新vscode插件

--- tools/vscode-extension/src/XMLDoc.js
+++ tools/vscode-extension/src/XMLDoc.js
@@ -2,6 +2,30 @@
 const xmlParser = require('xml-parser');
 var XMLWriter = require('xml-writer');
 
+/**
+ * 替换XMLWriter中的text方法，主要是为了避免被替换特殊字符
+ * @param {string} content 
+ */
+const NewTextFunc = function (content) {
+    if (!this.tags && !this.comment && !this.pi && !this.cdata) return this;
+    if (this.attributes && this.attribute) {
+        ++this.texts;
+        this.write(content);
+        return this;
+    } else if (this.attributes && !this.attribute) {
+        this.endAttributes();
+    }
+    if (this.comment || this.cdata) {
+        this.write(content);
+    }
+    else {
+        this.write(content.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+    }
+    ++this.texts;
+    this.started_write = true;
+    return this;
+};
+
 class XMLNode {
     /**
      * 
@@ -9,9 +33,9 @@ class XMLNode {
      * @returns {XMLNode}
      */
     static parse(xmlstring) {
-        let xml= xmlParser(xmlstring);
+        let xml = xmlParser(xmlstring);
         let xmldoc = null;
-        if(xml){
+        if (xml) {
             xmldoc = new XMLNode(xml.root);
         }
         return xmldoc;
@@ -21,16 +45,17 @@ class XMLNode {
      * 
      * @param {XMLNode} xmlnode 
      */
-    static write(xmlnode){
+    static write(xmlnode) {
         const xw = new XMLWriter();
+        xw.text = NewTextFunc;
         xw.startDocument();
         xmlnode.write(xw);
         xw.endDocument();
         return xw.toString();
     }
-   
+
     constructor(xmlNode) {
-        if(xmlNode === undefined){
+        if (xmlNode === undefined) {
             console.log();
         }
         this.name = xmlNode.name;
@@ -38,13 +63,13 @@ class XMLNode {
         this.parentNode = null;
         this.attributes = xmlNode.attributes;
         this.content = xmlNode.content;
-        if(xmlNode.children){
+        if (xmlNode.children) {
             this._parseSubNodes(xmlNode.children);
         }
     }
 
-    _parseSubNodes(nodes){
-        nodes.forEach(node=>{
+    _parseSubNodes(nodes) {
+        nodes.forEach(node => {
             const sub = new XMLNode(node);
             sub.parentNode = this;
             this.subNodes.push(sub);
@@ -55,17 +80,17 @@ class XMLNode {
      * 
      * @param {XMLWriter} xw 
      */
-    write(xw){
+    write(xw) {
         xw.startElement(this.name);
-        Object.keys(this.attributes).forEach(k=>{
+        Object.keys(this.attributes).forEach(k => {
             xw.writeAttribute(k, this.attributes[k]);
         });
 
-        if(this.content && this.content.length>0){
+        if (this.content && this.content.length > 0) {
             xw.text(this.content);
         }
 
-        this.subNodes.forEach(node=>{
+        this.subNodes.forEach(node => {
             node.write(xw);
         });
         xw.endElement();
